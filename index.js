6acb927940d641b71b48a54d617a01a519290104
Buxin-Emilo <zgsxiaohao@gmail.com>
2025-11-30 23:03:10 +0100

    new01.02

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 // 尝试加减法
-let ok = "hello";
-alert(ok);
-let x = 5;
-let y = 6;
-let z = x+y;
-alert(z);
+// let ok = "hello";
+// alert(ok);
+// let x = 5;
+// let y = 6;
+// let z = x+y;
+// alert(z);
+let name1 = 'john';
+let admin;
+admin = name1;
+alert(admin);
+let OURPLANET = 'earth';
+alert(OURPLANET);
+
+let name2 ='wocao';
+alert(`wo de min zi shi ${name2} `);
+alert(`tengo ${6+90} yuan`);
+
+let age = 88;
+alert(`wo you ${11+age} sui `)
