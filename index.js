715310ee3aeba8d9825bdac5f5901094236b5ff9
Buxin-Emilo <zgsxiaohao@gmail.com>
2025-12-01 20:05:45 +0100

    new01.03

--- index.js
+++ index.js
@@ -5,16 +5,79 @@
 // let y = 6;
 // let z = x+y;
 // alert(z);
-let name1 = 'john';
-let admin;
-admin = name1;
-alert(admin);
-let OURPLANET = 'earth';
-alert(OURPLANET);
+// let name1 = 'john';
+// let admin;
+// admin = name1;
+// alert(admin);
+// let OURPLANET = 'earth';
+// alert(OURPLANET);
 
-let name2 ='wocao';
-alert(`wo de min zi shi ${name2} `);
-alert(`tengo ${6+90} yuan`);
+// let name2 ='wocao';
+// alert(`wo de min zi shi ${name2} `);
+// alert(`tengo ${6+90} yuan`);
 
-let age = 88;
-alert(`wo you ${11+age} sui `)
+// let age = 88;
+// alert(`wo you ${11+age} sui `)
+// let num1 = 10;
+// let num2 = 20;
+// let sum = num1 + num2;
+
+// let lianNing;
+// alert(lianNing);
+// typeof 423;
+// alert(typeof 423);
+
+// let wde1lianning =prompt('请输入你的age',50);
+// alert(`ya tengo ${wde1lianning} anos`);
+ 
+// typeof 5555;
+// alert(typeof 5555);
+
+
+// let year = prompt('which year is your birthday?',1999);
+// alert(`your birthday is ${year} of this planet`);
+// let money = confirm('are you alive?');
+// alert(money);
+
+// 转换类型,数字转字符串
+// let minZi = 666666;
+// alert(typeof minZi);
+// minZi = String(minZi);
+// alert(typeof minZi);
+
+// 字符串转数字
+// let age1 = '25';
+// alert(typeof age1);
+// name2 = Number(age1);
+// alert(typeof name2);
+
+// 不需要重新声明变量，直接修改变量的属性。
+// let age2 = '25';
+// age2 = Number(age2);
+// alert(typeof age2);
+// // alert(Boolean(0)); // false
+// alert(Boolean('0')); // true
+// alert(Boolean('')); // false
+// alert(Boolean(null)); // false
+// alert(Boolean(undefined)); // false
+// alert(Boolean(NaN)); // false
+// alert(Boolean(' ')); // true
+// age2 = Boolean(age2);
+// alert(age2); // true
+
+// let age3 = 99;
+// age3 = -age3;
+// alert(age3); // -99
+
+// let age4 = 99, age5 = 100;
+// alert(age4 - age5); // -1
+
+// let age6 = 9, age7 = 2;
+// alert(age6 / age7); // 4
+// alert(age6 % age7); // 1
+// alert(age6 ** age7); // 81
+
+// let   age8 = '100', age9 = '20';
+// // alert(age8 + age9); // 10020
+// let age10 = age8 + age9;
+// alert(age10); 还是得到10020
