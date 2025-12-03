ff9efc4d959d3650ec7029926821bba6f2fa68cd
Buxin-Emilo <zgsxiaohao@gmail.com>
2025-12-03 23:00:00 +0100

    new01.10

--- index.js
+++ index.js
@@ -227,3 +227,18 @@
 //      );
 //      i++;
 // };
+let a = prompt('a?','');
+switch(a){
+    case '0':
+        alert('0');
+        break
+    case '1':
+        alert('1');
+        break
+        case '2':
+            case '3':
+                alert('2,3') ;
+                break;
+            default:
+    alert( 'We hope that this page looks ok!' ) 
+};
